1c5fe67ca93b0c5dcd252d94ea4dff1c13fce044
devdarkco <devdarkco@gmail.com>
2019-06-23 00:26:36 +0100

    fixed some bugs

--- src/main/java/com/devdarkco/rnd/util/ParticleCreator.java
+++ src/main/java/com/devdarkco/rnd/util/ParticleCreator.java
@@ -13,33 +13,26 @@ import java.util.Random;
 public class ParticleCreator {
 
     @SideOnly(Side.CLIENT)
-    public static void spawnParticle(EnumParticleTypes particle, World worldIn, BlockPos pos, int amount, Random rnd)
-    {
-        if (amount == 0)
-        {
-            amount = 15;
+    public static void spawnParticle(EnumParticleTypes particle, World worldIn, BlockPos pos, int amount, Random rnd) {
+        if (amount == 0) {
+            amount = 1;
         }
 
         IBlockState iblockstate = worldIn.getBlockState(pos);
 
-        if (iblockstate.getMaterial() != Material.AIR)
-        {
-            for (int i = 0; i < amount; ++i)
-            {
+        if (iblockstate.getMaterial() != Material.AIR) {
+            for (int i = 0; i < amount; ++i) {
                 double d0 = rnd.nextGaussian() * 0.02D;
                 double d1 = rnd.nextGaussian() * 0.02D;
                 double d2 = rnd.nextGaussian() * 0.02D;
                 worldIn.spawnParticle(particle, (double)((float)pos.getX() + rnd.nextFloat()), (double)pos.getY() + (double)rnd.nextFloat() * iblockstate.getBoundingBox(worldIn, pos).maxY, (double)((float)pos.getZ() + rnd.nextFloat()), d0, d1, d2);
             }
-        }
-        else
-        {
-            for (int i1 = 0; i1 < amount; ++i1)
-            {
+        } else {
+            for (int i1 = 0; i1 < amount; ++i1) {
                 double d0 = rnd.nextGaussian() * 0.02D;
                 double d1 = rnd.nextGaussian() * 0.02D;
                 double d2 = rnd.nextGaussian() * 0.02D;
-                worldIn.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, (double)((float)pos.getX() + rnd.nextFloat()), (double)pos.getY() + (double)rnd.nextFloat() * 1.0f, (double)((float)pos.getZ() + rnd.nextFloat()), d0, d1, d2, new int[0]);
+                worldIn.spawnParticle(particle, (double)((float)pos.getX() + rnd.nextFloat()), (double)pos.getY() + (double)rnd.nextFloat() * 1.0f, (double)((float)pos.getZ() + rnd.nextFloat()), d0, d1, d2, new int[0]);
             }
         }
     }
