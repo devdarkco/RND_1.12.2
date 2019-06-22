7b59033bf17b4a80b923b2538739b6436f6f70c3
devdarkco <devdarkco@gmail.com>
2019-06-22 17:59:29 +0100

    im lazy af

---
+++ src/main/java/com/devdarkco/rnd/util/ChatUtil.java
@@ -0,0 +1,11 @@
+package com.devdarkco.rnd.util;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextComponentString;
+
+public class ChatUtil {
+
+    public static void  chatMessage(EntityPlayer player, String text){
+        player.sendMessage(new TextComponentString(text));
+    }
+}
