9e5dce4532422d12b275fa9375e39ddd725ac223
devdarkco <devdarkco@gmail.com>
2019-07-08 03:19:34 +0100

    added the growth pedestal

--- src/main/java/com/devdarkco/rnd/init/RndBlocks.java
+++ src/main/java/com/devdarkco/rnd/init/RndBlocks.java
@@ -1,5 +1,6 @@
 package com.devdarkco.rnd.init;
 
+import com.devdarkco.rnd.init.blocks.BlockGrowthPedestal;
 import com.devdarkco.rnd.init.blocks.BlockPoweredDirt;
 import com.devdarkco.rnd.init.blocks.RndBlock;
 import net.minecraft.block.Block;
@@ -12,6 +13,6 @@ public class RndBlocks {
 
     public static final List<Block> BLOCKS = new ArrayList<Block>();
 
-    public static final Block RND_GENERATOR = new RndBlock(Material.ROCK, "generator");
     public static final Block POWERED_DIRT = new BlockPoweredDirt(Material.GROUND, "powered_dirt");
+    public static final Block GROWTH_PEDESTAL = new BlockGrowthPedestal(Material.GROUND, "growth_pedestal");
 }
