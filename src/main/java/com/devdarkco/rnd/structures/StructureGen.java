ebe9728c48b50f8e4f65418cf55c962b53241e61
devdarkco <devdarkco@gmail.com>
2019-07-08 03:20:57 +0100

    structure gen based on cjminecraft tutorial, just for test

---
+++ src/main/java/com/devdarkco/rnd/structures/StructureGen.java
@@ -0,0 +1,44 @@
+package com.devdarkco.rnd.structures;
+
+import java.util.Random;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraftforge.fml.common.IWorldGenerator;
+
+public class StructureGen implements IWorldGenerator {
+
+    private StructureGenerator cabinGenerator;
+
+    public StructureGen() {
+        this.cabinGenerator = new StructureGenerator("mod_hut");
+    }
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+        int blockX = chunkX * 16 + random.nextInt(16);
+        int blockZ = chunkZ * 16 + random.nextInt(16);
+
+        switch (world.provider.getDimension()) {
+            case 0:
+                // The overworld
+                runGenerator(this.cabinGenerator, world, blockX, blockZ, 50, random);
+                break;
+            case -1:
+                // The nether
+                break;
+            case 1:
+                // The end
+                break;
+        }
+    }
+
+    private void runGenerator(StructureGenerator generator, World world, int blockX, int blockZ, int chance, Random random) {
+        if ((int) (Math.random() * chance) == 0) {
+            generator.generate(world, random, new BlockPos(blockX, StructureGenerator.getGroundFromAbove(world, blockX, blockZ), blockZ));
+        }
+    }
+
+}
